a1e6cab7f58a63a08e22aaed743be67449c61e9c
James Jaramillo <jjaram53@uniminuto.edu.co>
2026-05-20 19:51:58 -0500

    Fix: Se agrega inicialización automática de la BD para despliegues en la nube

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import sqlite3
 import pandas as pd
 import time
 from datetime import datetime
+import os                     
+from init_db import inicializar_base_datos  
 
 DB_NAME = "turnoya.db"
 
+if not os.path.exists(DB_NAME):
+    inicializar_base_datos()
+    
 def get_db_connection():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
